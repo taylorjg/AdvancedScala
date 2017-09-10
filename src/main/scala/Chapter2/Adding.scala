cad71fc9ffebf3421c190630a9548af2c93f832a
Jon Taylor <jont@cakesolutions.net>
2017-09-10 18:31:27 +0100

    Done third part of third exercise of chapter 2

--- src/main/scala/Chapter2/Adding.scala
+++ src/main/scala/Chapter2/Adding.scala
@@ -8,11 +8,26 @@ import cats.syntax.option._
 object Adding extends App {
 
   val xs1 = List(1, 2, 3, 4, 5)
-  val xs2 = xs1.map(_.some)
   println(s"add($xs1): ${add(xs1)}")
+
+  val xs2 = xs1.map(_.some)
   println(s"add($xs2): ${add(xs2)}")
+
+  import MonoidInstances.orderMonoid
+  val orders = List(Order(100, 2), Order(500, 5))
+  println(s"add($orders): ${add(orders)}")
 
   def add[A](items: List[A])(implicit m: Monoid[A]): A = {
     items.foldLeft(m.empty)(m.combine)
+  }
+
+  case class Order(totalCost: Double, quantity: Double)
+
+  object MonoidInstances {
+    implicit val orderMonoid: Monoid[Order] = new Monoid[Order] {
+      override def combine(x: Order, y: Order) =
+        Order(x.totalCost + y.totalCost, x.quantity + y.quantity)
+      override def empty = Order(0, 0)
+    }
   }
 }
