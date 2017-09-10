5f2fd65c361532c501da0f987c0e55b1bcd63d57
Jon Taylor <jont@cakesolutions.net>
2017-09-10 18:21:22 +0100

    Done second part of third exercise of chapter 2

--- src/main/scala/Chapter2/Adding.scala
+++ src/main/scala/Chapter2/Adding.scala
@@ -2,14 +2,17 @@ package Chapter2
 
 import cats.Monoid
 import cats.instances.int._
+import cats.instances.option._
+import cats.syntax.option._
 
 object Adding extends App {
 
-  val xs = List(1, 2, 3, 4, 5)
-  println(s"add($xs): ${add(xs)}")
+  val xs1 = List(1, 2, 3, 4, 5)
+  val xs2 = xs1.map(_.some)
+  println(s"add($xs1): ${add(xs1)}")
+  println(s"add($xs2): ${add(xs2)}")
 
-  def add(items: List[Int]): Int = {
-    val m = Monoid[Int]
+  def add[A](items: List[A])(implicit m: Monoid[A]): A = {
     items.foldLeft(m.empty)(m.combine)
   }
 }
